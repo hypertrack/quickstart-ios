b440205994661d8b1d109b06027f595f9696aa7f
Eugene Tulushev <eugene@tulushev.com>
2019-02-26 20:58:43 +0200

    Create basic iOS app with TabBar template

---
+++ Quickstart/Quickstart/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Quickstart
+//
+//  Created by Eugene Tulushev on 26.02.19.
+//  Copyright © 2019 HyperTrack Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
