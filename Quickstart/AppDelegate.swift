e1da37a1bab8851ac8fc0981ac9d29020a2460db
Eugene Tulushev <eugene@tulushev.com>
2019-02-27 16:58:30 +0200

    Port selected functionality from TestApp to Quickstart

--- Quickstart/AppDelegate.swift
+++ Quickstart/AppDelegate.swift
@@ -9,13 +9,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        HyperTrackCore.initialize(publishableKey: "pk_06f18a899893ad7588a9294fe2ead708946c16c0") { (error) in
-            /// perform post initialization actions
-            /// handle error if any
-            print(error?.errorMessage ?? "initialized")
-        }
+        initializeHyperTrackSDK()
         
         return true
+    }
+    
+    func initializeHyperTrackSDK() {
+        HyperTrackCore.initialize(publishableKey: "pk_06f18a899893ad7588a9294fe2ead708946c16c0") { (error) in
+            
+            if let message = error?.errorMessage {
+                DispatchQueue.main.async { [weak self] in
+                    let alert = UIAlertController(title: "Failed to register this device", message: message, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Retry", style: UIAlertAction.Style.default, handler: { [weak self] _ in
+                        self?.initializeHyperTrackSDK()
+                    }))
+                    self?.window?.rootViewController?.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
 }
 
--- Quickstart/ViewController.swift
+++ Quickstart/ViewController.swift
@@ -3,14 +3,78 @@ import HyperTrackCore
 
 class ViewController: UIViewController {
     
+    @IBOutlet var deviceID: UILabel!
+    @IBOutlet var locationPermissionButton: UIButton!
+    @IBOutlet var activityPermissionButton: UIButton!
+    @IBOutlet var trackingButton: UIButton!
+    
+    var trackingEnabled = false {
+        didSet {
+            updateTrackingButtonTitle()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        HyperTrackCore.requestLocationPermission { (error) in
-            print(error?.errorMessage ?? "location permissions granted")
+        HyperTrackCore.setServiceStatusUpdatesDelegate(self)
+        
+        deviceID.text = HyperTrackCore.getDeviceId()
+    }
+    
+    @IBAction func locationPermissionButtonClicked() {
+        locationPermissionButton.isUserInteractionEnabled = false
+        HyperTrackCore.requestLocationPermission(completionHandler: nil)
+    }
+    
+    @IBAction func activityPermissionButtonClicked() {
+        activityPermissionButton.isUserInteractionEnabled = false
+        HyperTrackCore.requestActivityPermission(completionHandler: nil)
+    }
+    
+    
+    @IBAction func resumeTrackingButtonClicked() {
+        if trackingEnabled {
+            trackingEnabled = false
+            HyperTrackCore.pauseTracking()
+        } else {
+            HyperTrackCore.resumeTracking()
         }
-        HyperTrackCore.requestActivityPermission { (error) in
-            print(error?.errorMessage ?? "motion permissions granted")
+        updateTrackingButtonTitle()
+    }
+    
+    func updateTrackingButtonTitle() {
+        trackingButton.setTitle(trackingEnabled ? "Pause Tracking" : "Resume Tracking", for: .normal)
+    }
+}
+
+
+extension ViewController: ServiceStatusUpdateDelegate {
+    
+    func serviceStatusUpdated(_ type: Config.Services.ServiceType, status: ServiceStatus) {
+        let serviceEnabled = status == .started ? true : false
+        switch type {
+        case .activity:
+            let title = (serviceEnabled ? "Activity Service Running" : (HyperTrackCore.checkActivityPermission() ? "Activity Service Paused" : "Enable Activity Permission"))
+            updateServiceButtonState(button: activityPermissionButton, serviceEnabled: serviceEnabled, title: title)
+        case .location:
+            let title = (serviceEnabled ? "Location Service Running" : (HyperTrackCore.checkLocationPermission() ? "Location Service Paused" : "Enable Location Permission"))
+            updateServiceButtonState(button: locationPermissionButton, serviceEnabled: serviceEnabled, title: title)
+        default:
+            break
         }
+    }
+    
+    func updateServiceButtonState(button: UIButton, serviceEnabled: Bool, title: String) {
+        if serviceEnabled {
+            button.backgroundColor = UIColor(red: 0.00, green: 0.79, blue: 0.29, alpha: 1.00)
+        } else {
+            button.backgroundColor = UIColor.black
+        }
+        let enableButton = self.trackingEnabled || serviceEnabled
+        button.setTitle(title, for: .normal)
+        button.isUserInteractionEnabled = !serviceEnabled
+        
+        self.trackingEnabled = (enableButton && HyperTrackCore.checkActivityPermission() && HyperTrackCore.checkLocationPermission())
     }
 }
