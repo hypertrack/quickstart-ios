66a297e5b6c591d3d4afffb13a0f974eb205c983
Eugene Tulushev <eugene@tulushev.com>
2019-02-27 14:36:03 +0200

    Setup descriptions and code to request location permissions

--- Quickstart/ViewController.swift
+++ Quickstart/ViewController.swift
@@ -1,8 +1,16 @@
 import UIKit
+import HyperTrackCore
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        HyperTrackCore.requestLocationPermission { (error) in
+            print(error?.errorMessage ?? "error requesting location permissions")
+        }
+        HyperTrackCore.requestActivityPermission { (error) in
+            print(error?.errorMessage ?? "error requestiong motion permissions")
+        }
     }
 }
