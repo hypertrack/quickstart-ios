120fec5fb19594cae9dbe298b2af2ebcea1a23df
Dmytro Shapovalov <dshtorm@gmail.com>
2020-12-23 10:21:45 +0100

    Update to iOS SDK 4.7.0

--- UIKit/Quickstart/ViewController.swift
+++ UIKit/Quickstart/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    
     /// Register for notifications to update the tracking indicator state
     NotificationCenter.default.addObserver(
       self,
